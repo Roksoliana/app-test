fdb924b74c9647232cf738eba0458ba4f5b72549
Roksoliana <yatsunda7@gmail.com>
2019-06-05 13:34:39 +0300

    f

--- src/app/user/modal-add-user/input/input.component.ts
+++ src/app/user/modal-add-user/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { User } from 'src/app/user/user';
 
 @Component({
@@ -10,9 +11,31 @@ export class InputComponent implements OnInit {
 
   @Input() newUser: User;
   @Input() edit: boolean;
-  constructor() { }
+
+  public userForm: FormGroup;
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+  private initForm(): void {
+    this.userForm = this.formbuilder.group({
+      name: new FormControl(this.newUser.name, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      surname: new FormControl(this.newUser.surname, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      email: new FormControl(this.newUser.email, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      age: new FormControl(this.newUser.age, [
+        Validators.required
+      ])
+    });
   }
 
 }
